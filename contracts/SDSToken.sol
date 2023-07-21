2277f6578983d89e84320742c776421835916d7d
Doriav <doriav20@gmail.com>
2023-07-21 23:07:15 +0300

    Add mint500 function for testing purposes only

--- contracts/SDSToken.sol
+++ contracts/SDSToken.sol
@@ -5,4 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SDSToken is ERC20 {
     constructor() ERC20("SDSToken", "SDS") {}
+
+    function mint500() public {
+        _mint(msg.sender, 500);
+    }
 }
