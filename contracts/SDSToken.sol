21243525e30734befc2ad4c460657adcf9dae0c2
Doriav <doriav20@gmail.com>
2023-07-21 23:36:58 +0300

    Address for minting

--- contracts/SDSToken.sol
+++ contracts/SDSToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract SDSToken is ERC20 {
     constructor() ERC20("SDSToken", "SDS") {}
 
-    function mint500() public {
-        _mint(msg.sender, 500);
+    function mint500(address to) public {
+        _mint(to, 500);
     }
 }
