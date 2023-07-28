b8b45e3c07bf80cd2e9d012e2a264e3af395ba50
BBB101 <104635676+BBB101@users.noreply.github.com>
2023-07-28 17:08:59 +0300

    Support Ownable (#8)

--- contracts/SDSToken.sol
+++ contracts/SDSToken.sol
@@ -2,12 +2,13 @@
 pragma solidity ^0.8.9;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SDSToken is ERC20 {
+contract SDSToken is ERC20, Ownable {
     constructor() ERC20("SDSToken", "SDS") {}
 
     // Note: this function is only for testing purposes
-    function mint500(address to) public {
+    function mint500(address to) public onlyOwner {
         _mint(to, 500);
     }
 }
