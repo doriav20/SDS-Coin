d4bfdea064da664fd1cc44d5b2816a66f94983cd
BBB101 <timeninja343@gmail.com>
2023-07-28 16:05:28 +0300

    Fix format because doriav

--- contracts/SDSToken.sol
+++ contracts/SDSToken.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.9;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SDSToken is ERC20,Ownable {
     constructor() ERC20("SDSToken", "SDS") {}
