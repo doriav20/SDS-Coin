f3bf60f4b54b7697d2a2ebd32e7637f97a53c42c
doriav20 <doriav20@gmail.com>
2023-08-27 01:02:19 +0300

    Fetch decimals and display balance correctly

--- frontend/src/useMyBalance.js
+++ frontend/src/useMyBalance.js
@@ -4,6 +4,7 @@ import {ABI, CONTRACT_ADDRESS} from "./constants";
 
 export function useMyBalance() {
   const [balance, setBalance] = useState(null);
+  const [decimals, setDecimals] = useState(null);
 
   async function fetchBalance() {
     if (window.ethereum) {
@@ -17,21 +18,50 @@ export function useMyBalance() {
     }
   }
 
+  async function fetchDecimals() {
+    if (window.ethereum) {
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+      const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer);
+      const contractDecimals = await contract.decimals();
+      setDecimals(contractDecimals);
+    }
+  }
+
   useEffect(() => {
-    const intervalId = setInterval(fetchBalance, 5000);  // fetch every 5 seconds
+    fetchDecimals(); // Fetch decimals once during initial mount
+
+    const fiveSecondsInMilliseconds = 5000;
+    const intervalId = setInterval(fetchBalance, fiveSecondsInMilliseconds);  // fetch every 5 seconds
     return () => clearInterval(intervalId); // cleanup the interval on component unmount
   }, []);
 
-  return { balance, fetchBalance };
+  return { balance, fetchBalance, decimals };
 }
 
 export function BalanceDisplay() {
-  const { balance, fetchBalance } = useMyBalance();
+  const { balance, fetchBalance, decimals } = useMyBalance();
+
+  let readableBalance = null;
+
+  if (balance && decimals) {
+    const integerPart = balance / (10n ** decimals);
+    const fractionalPart = balance % (10n ** decimals);
+
+    // Convert fractionalPart to string and remove trailing zeros
+    let fractionalString = fractionalPart.toString().padStart(Number(decimals), '0');
+    while (fractionalString.endsWith('0') && fractionalString.length > 1) {
+        fractionalString = fractionalString.substring(0, fractionalString.length - 1);
+    }
+
+    readableBalance = `${integerPart}.${fractionalString}`;
+  }
+
 
   return (
     <div>
       <button onClick={fetchBalance}>Check Balance</button>
-      {balance && <p>Your balance: {balance.toString()} SDS</p>}
+      {readableBalance && <p>Your balance: {readableBalance} SDS</p>}
     </div>
   );
 }
