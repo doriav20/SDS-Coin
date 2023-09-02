da9c0749444be069ded2c1fc05d520fe8eaa937d
doriav20 <doriav20@gmail.com>
2023-09-02 17:35:29 +0300

    Add ethereum context

---
+++ frontend/src/hooks/useEthereum.js
@@ -0,0 +1,35 @@
+import {createContext, useContext, useEffect, useState} from 'react';
+import {ethers} from 'ethers';
+import {ABI, CONTRACT_ADDRESS} from "./constants";
+
+const EthereumContext = createContext(null);
+
+export function EthereumProvider({children}) {
+    const [provider, setProvider] = useState(null);
+    const [signer, setSigner] = useState(null);
+    const [contract, setContract] = useState(null);
+
+
+    useEffect(() => {
+        if (window.ethereum) {
+            const newProvider = new ethers.BrowserProvider(window.ethereum);
+            setProvider(newProvider);
+
+            newProvider.getSigner().then((newSigner) => {
+                setSigner(newSigner);
+                const newContract = new ethers.Contract(CONTRACT_ADDRESS, ABI, newSigner);
+                setContract(newContract);
+            });
+        }
+    }, []);
+
+    return (
+        <EthereumContext.Provider value={{provider, signer, contract}}>
+            {children}
+        </EthereumContext.Provider>
+    );
+}
+
+export function useEthereum() {
+    return useContext(EthereumContext);
+}
