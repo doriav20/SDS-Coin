92534aefffb1678c5301b0d05d42243860637b67
doriav20 <doriav20@gmail.com>
2023-09-11 23:39:23 +0300

    Add random generator

--- backend/contracts/SDSToken.sol
+++ backend/contracts/SDSToken.sol
@@ -7,6 +7,8 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 contract SDSToken is ERC20, Ownable {
     constructor() ERC20("SDSToken", "SDS") {}
 
+    uint256 private currRand = 0xDEADBEEF_CAFEBABE_ABADF00D_0D15EA5E;
+
     function mint(address to, uint256 amount) public onlyOwner {
         uint256 _amount = amount * (10 ** decimals());
         _mint(to, _amount);
@@ -18,5 +20,13 @@ contract SDSToken is ERC20, Ownable {
 
     function myBalance() public view returns (uint256) {
         return balanceOf(_msgSender());
+    }
+
+    function generateRandomNumber() public {
+        currRand = uint256(keccak256(abi.encodePacked(block.timestamp, block.prevrandao, block.number, currRand)));
+    }
+
+    function getRandomNumber() public view returns (uint256) {
+        return currRand;
     }
 }
