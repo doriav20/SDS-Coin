20a4930c3269e3059c58596e9ede289f277c594d
doriav20 <doriav20@gmail.com>
2023-09-14 13:56:27 +0300

    Implement game

--- backend/contracts/CoinFlipJackpot.sol
+++ backend/contracts/CoinFlipJackpot.sol
@@ -32,9 +32,74 @@ contract CoinFlipJackpot is Context, Randomizeble {
         sdsTokenContract = SDSToken(_sdsTokenContractAddress);
     }
 
-    function stakeSDS(uint8 _stakedAmount) external {}
+    function stakeSDS(uint8 _stakedAmount) external {
+        Player storage player = players[_msgSender()];
 
-    function playRound(bool _prediction) external {}
+        require(!player.inGame, "Game already in progress!");
+        require(block.timestamp - player.lastTimePlayed > 1 days, "You can only play once per day!");
+        require(sdsTokenContract.transferFrom(_msgSender(), address(this), _stakedAmount), "Transfer failed!");
+
+        uint8 _rounds = _stakedAmount;
+
+        player.stakedAmount = _stakedAmount;
+        player.totalRounds = _rounds;
+        player.jackpot = 0;
+        player.multiplier = 1;
+        player.roundsPlayed = 0;
+        player.lastTimePlayed = block.timestamp;
+        player.inGame = true;
+    }
+
+    function playRound(bool _prediction) external {
+        Player storage player = players[_msgSender()];
+
+        require(player.inGame, "Game not in progress!");
+        require(player.roundsPlayed < player.totalRounds, "All rounds played");
+
+        generateRandomNumber();
+        bool outcome = (getRandomNumber() % 2) == 0;
+
+        if (_prediction == outcome) {
+            if (player.jackpot == 0) {
+                player.jackpot = 2;
+            } else {
+                player.jackpot *= player.multiplier;
+                player.multiplier++;
+            }
+        } else {
+            player.jackpot = 0;
+            player.multiplier = 1;
+        }
+
+        player.roundsPlayed++;
+
+        emit RoundResult(_msgSender(), _prediction, outcome, player.jackpot);
+
+        if (player.roundsPlayed == player.totalRounds) {
+            emit GameEnded(_msgSender(), player.jackpot);
+            gameFinishedHandler();
+        }
+    }
+
+    function gameFinishedHandler() private {
+        Player storage player = players[_msgSender()];
+        require(player.roundsPlayed == player.totalRounds, "Not all rounds played");
+
+        uint256 amount = player.jackpot;
+        require(sdsTokenContract.transfer(_msgSender(), amount), "Transfer failed!");
+
+        resetPlayerData();
+    }
+
+    function resetPlayerData() private {
+        Player storage player = players[_msgSender()];
+        player.stakedAmount = 0;
+        player.jackpot = 0;
+        player.multiplier = 1;
+        player.totalRounds = 0;
+        player.roundsPlayed = 0;
+        player.inGame = false;
+    }
 
     function getGameStatus() external view returns (GameStatus) {
         Player storage player = players[_msgSender()];
