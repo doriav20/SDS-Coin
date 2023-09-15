f7b381bfdcf0048251b5cc1d90abf5e9a6d71c90
doriav20 <doriav20@gmail.com>
2023-09-16 00:30:20 +0300

    Fix prettier

--- backend/contracts/Mintable.sol
+++ backend/contracts/Mintable.sol
@@ -6,7 +6,6 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { OneTimeCallable } from "./OneTimeCallable.sol";
 
 abstract contract Mintable is ERC20, Ownable, OneTimeCallable {
-
     function mint(address to, uint256 amount) public onlyOwner {
         uint256 _amount = amount * (10 ** decimals());
         _mint(to, _amount);
