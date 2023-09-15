b0ce631b6099ff75419f32f972cc439dc55aefb3
doriav20 <doriav20@gmail.com>
2023-09-16 01:38:05 +0300

    Check if function called by its name

--- backend/contracts/OneTimeCallable.sol
+++ backend/contracts/OneTimeCallable.sol
@@ -13,4 +13,13 @@ abstract contract OneTimeCallable {
         _;
         _hasFunctionBeenCalled[msg.sender][msg.sig] = true;
     }
+
+    function hasFunctionBeenCalledOnce(string memory functionSignature) public view returns (bool) {
+        bytes4 functionSig = bytes4(keccak256(abi.encodePacked(functionSignature)));
+        return _hasFunctionBeenCalledOnce(msg.sender, functionSig);
+    }
+
+    function _hasFunctionBeenCalledOnce(address caller, bytes4 functionSig) private view returns (bool) {
+        return _hasFunctionBeenCalled[caller][functionSig];
+    }
 }
