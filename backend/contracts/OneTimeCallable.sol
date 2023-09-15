ac20f6fa72f2938b6b786741f140e452c5b6fb60
doriav20 <doriav20@gmail.com>
2023-09-16 01:37:33 +0300

    Make hasFunctionBeenCalled private

--- backend/contracts/OneTimeCallable.sol
+++ backend/contracts/OneTimeCallable.sol
@@ -4,13 +4,13 @@ pragma solidity ^0.8.19;
 error FunctionCanOnlyBeCalledOnceByTheCaller();
 
 abstract contract OneTimeCallable {
-    mapping(address => mapping(bytes4 => bool)) public hasFunctionBeenCalled;
+    mapping(address => mapping(bytes4 => bool)) private _hasFunctionBeenCalled;
 
     modifier canCallOnce() {
-        if (hasFunctionBeenCalled[msg.sender][msg.sig]) {
+        if (_hasFunctionBeenCalled[msg.sender][msg.sig]) {
             revert FunctionCanOnlyBeCalledOnceByTheCaller();
         }
         _;
-        hasFunctionBeenCalled[msg.sender][msg.sig] = true;
+        _hasFunctionBeenCalled[msg.sender][msg.sig] = true;
     }
 }
