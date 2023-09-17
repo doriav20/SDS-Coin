b4191a71899d726a9f2efed207f86329807e16ef
doriav20 <doriav20@gmail.com>
2023-09-18 02:24:22 +0300

    Refactor

--- frontend/src/hooks/useEthereum.js
+++ frontend/src/hooks/useEthereum.js
@@ -10,24 +10,23 @@ export function EthereumProvider({ children }) {
     const [contract, setContract] = useState(null);
 
     useEffect(() => {
-        if (window.ethereum) {
-            const newProvider = new ethers.JsonRpcProvider();
-            setProvider(newProvider);
+        // const newProvider = new ethers.BrowserProvider(window.ethereum); // This is browser provider (MetaMask for example)
+        const newProvider = new ethers.JsonRpcProvider();
+        setProvider(newProvider);
 
-            newProvider
-                .getSigner()
-                .then((newSigner) => {
-                    setSigner(newSigner);
-                    const newContract = new ethers.Contract(CONTRACT_ADDRESS, ABI, newSigner);
-                    setContract(newContract);
-                })
-                .catch(() => {
-                    alert('Please allow access for the app to work');
-                    setProvider(null);
-                    setSigner(null);
-                    setContract(null);
-                });
-        }
+        newProvider
+            .getSigner()
+            .then((newSigner) => {
+                setSigner(newSigner);
+                const newContract = new ethers.Contract(CONTRACT_ADDRESS, ABI, newSigner);
+                setContract(newContract);
+            })
+            .catch(() => {
+                alert('Please allow access for the app to work');
+                setProvider(null);
+                setSigner(null);
+                setContract(null);
+            });
     }, []);
 
     return <EthereumContext.Provider value={{ provider, signer, contract }}>{children}</EthereumContext.Provider>;
