d9543038926e6575bb5c6af32fa63edec3761968
BBB101 <timeninja343@gmail.com>
2023-09-19 22:32:05 +0300

    Run Prettier on Merged

--- backend/contracts/SDSToken.sol
+++ backend/contracts/SDSToken.sol
@@ -7,16 +7,15 @@ import { Randomizeble } from "./Randomizable.sol";
 import { TicTacToePlayable } from "./TicTacToe/TicTacToePlayable.sol";
 import { RouletteGame } from "./Roulette.sol";
 
-contract SDSToken is SDSTokenStandard, Mintable, Randomizeble, TicTacToePlayable {    
+contract SDSToken is SDSTokenStandard, Mintable, Randomizeble, TicTacToePlayable {
     RouletteGame public rouletteContract;
 
-    constructor() 
-    {
+    constructor() {
         rouletteContract = new RouletteGame(address(this));
         addOwner(address(ticTacToeContract));
         addOwner(address(rouletteContract));
     }
-    
+
     function playRoulette(uint256 amountOfRed, uint256 amountOfGreen, uint256 amountOfBlack) external {
         require(balanceOf(msg.sender) >= amountOfRed + amountOfGreen + amountOfBlack, "You don't have enough tokens");
         rouletteContract.playR(amountOfRed, amountOfGreen, amountOfBlack, msg.sender);
