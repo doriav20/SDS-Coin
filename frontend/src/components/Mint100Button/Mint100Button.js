94591520c03d8120db2404a0d032ec778c4023e7
Doriav <doriav20@gmail.com>
2023-09-21 12:01:33 +0300

    Hide right after clicked

--- frontend/src/components/Mint100Button/Mint100Button.js
+++ frontend/src/components/Mint100Button/Mint100Button.js
@@ -1,12 +1,22 @@
 import './Mint100Button.css';
 import useMint100 from '../../hooks/useMint100';
+import { useState } from 'react';
 
 function MintButton({ ...props }) {
     const { canMint100, mint100 } = useMint100();
+    const [clicked, setClicked] = useState(false);
 
     return (
+        !clicked &&
         canMint100 && (
-            <button {...props} className="mint100-button" onClick={mint100}>
+            <button
+                {...props}
+                className="mint100-button"
+                onClick={() => {
+                    setClicked(true);
+                    mint100();
+                }}
+            >
                 Mint 100 SDS
             </button>
         )
