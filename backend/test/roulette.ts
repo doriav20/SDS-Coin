92250aa201487f4e368d7bc90987a4ce0c29df0a
doriav20 <doriav20@gmail.com>
2023-09-23 01:25:14 +0300

    Refactor test

--- backend/test/roulette.ts
+++ backend/test/roulette.ts
@@ -47,20 +47,28 @@ describe("SDSToken", function () {
         }
     });
     it("When betting on both red and black nothing the balance shouldn't change or lose twice the amount", async function () {
-        const amount_of_retries = 5;
-        const bettedAmountOnRed = 10n;
-        const bettedAmountOnGreen = 0n;
-        const bettedAmountOnBlack = 10n;
+        const amountOfRetries = 5;
 
-        for (let i = 0; i < amount_of_retries; i++) {
-            const balance_before_play = await instance.connect(player1).myBalance();
-            await instance.connect(player1).playRoulette(bettedAmountOnRed, bettedAmountOnGreen, bettedAmountOnBlack);
-            const balance_after_play = await instance.connect(player1).myBalance();
-            const resultColor = await instance.connect(player1).getRouletteResultForPlayer();
-            if (resultColor !== Color.GREEN) {
-                expect(balance_before_play).to.equal(balance_after_play); //
-            } else {
-                expect(balance_before_play - bettedAmountOnRed - bettedAmountOnBlack).to.equal(balance_after_play);
+        const betAmountOnRed = 10n;
+        const betAmountOnGreen = 0n;
+        const betAmountOnBlack = 10n;
+
+        for (let i = 0; i < amountOfRetries; i++) {
+            const balanceBeforePlay = await instance.connect(player1).myBalance();
+            await instance.connect(player1).playRoulette(betAmountOnRed, betAmountOnGreen, betAmountOnBlack);
+            const balanceAfterPlay = await instance.connect(player1).myBalance();
+            const resultColor = Number(await instance.connect(player1).getRouletteResultForPlayer());
+
+            switch (resultColor) {
+                case Number(Color.RED):
+                case Number(Color.BLACK):
+                    expect(balanceBeforePlay).to.equal(balanceAfterPlay);
+                    break;
+                case Number(Color.GREEN):
+                    expect(balanceBeforePlay - betAmountOnRed - betAmountOnBlack).to.equal(balanceAfterPlay);
+                    break;
+                default:
+                    throw new Error("Unknown color");
             }
         }
     });
